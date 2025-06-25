7960ba36f4feebbc9778068800245fe9967addc2
Mustitz <mustitz@gmail.com>
2025-06-25 12:47:03 +0000

    Port C definitions from msquic.h.

--- build.zig
+++ build.zig
@@ -4,6 +4,11 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const quarkz_dep = b.dependency("quarkz", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "msquiczig",
         .root_source_file = b.path("src/lib.zig"),
@@ -15,6 +20,9 @@ pub fn build(b: *std.Build) void {
 
     _ = b.addModule("msquiczig", .{
         .root_source_file = b.path("src/lib.zig"),
+        .imports = &.{
+            .{ .name = "quarkz", .module = quarkz_dep.module("quarkz") },
+        },
     });
 
     const tests = b.addTest(.{
@@ -23,6 +31,8 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .link_libc = true,
     });
+
+    tests.root_module.addImport("quarkz", quarkz_dep.module("quarkz"));
 
     const run_tests = b.addRunArtifact(tests);
     const test_step = b.step("test", "Run library tests");
