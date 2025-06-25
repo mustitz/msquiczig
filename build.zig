8aef9cd0e6386bad539852055831cc5c7ae26ed3
Mustitz <mustitz@gmail.com>
2025-06-25 14:47:57 +0000

    Add client/server examples.

--- build.zig
+++ build.zig
@@ -18,12 +18,48 @@ pub fn build(b: *std.Build) void {
     });
     b.installArtifact(lib);
 
-    _ = b.addModule("msquiczig", .{
+    const msquiczig = b.addModule("msquiczig", .{
         .root_source_file = b.path("src/lib.zig"),
         .imports = &.{
             .{ .name = "quarkz", .module = quarkz_dep.module("quarkz") },
         },
     });
+
+    const server_example = b.addExecutable(.{
+        .name = "msquic-server",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("examples/server/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+            .imports = &.{
+                .{ .name = "quarkz", .module = quarkz_dep.module("quarkz") },
+                .{ .name = "msquiczig", .module = msquiczig },
+            },
+        }),
+    });
+
+    const client_example = b.addExecutable(.{
+        .name = "msquic-client",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("examples/client/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+            .imports = &.{
+                .{ .name = "quarkz", .module = quarkz_dep.module("quarkz") },
+                .{ .name = "msquiczig", .module = msquiczig },
+            },
+        }),
+    });
+
+    const run_server = b.addRunArtifact(server_example);
+    const run_server_step = b.step("run-server", "Run the server app");
+    run_server_step.dependOn(&run_server.step);
+
+    const run_client = b.addRunArtifact(client_example);
+    const run_client_step = b.step("run-client", "Run the client app");
+    run_client_step.dependOn(&run_client.step);
 
     const tests = b.addTest(.{
         .root_source_file = b.path("src/lib.zig"),
---
+++ examples/server/main.zig
@@ -0,0 +1,108 @@
+const std = @import("std");
+const quarkz = @import("quarkz");
+const msquiczig = @import("msquiczig");
+
+const Allocator = std.mem.Allocator;
+const Location = std.builtin.SourceLocation;
+
+const Atom = quarkz.Atom;
+const Cosmos = quarkz.Cosmos;
+const MsQuic = msquiczig.MsQuic;
+
+var stdout_tracer = quarkz.cosmos.FileRecorder{
+    .min_level = .trace,
+};
+
+const Server = struct {
+    allocator: Allocator,
+    cosmos: *Cosmos,
+    msquic: *MsQuic,
+
+    fn init(allocator: Allocator) !Server {
+        const new_cosmos = try Cosmos.create(allocator);
+        errdefer new_cosmos.destroy();
+
+        try new_cosmos.addRecorder(stdout_tracer.get());
+
+        const atom_name = "Server.init";
+        const atom: ?*Atom = new_cosmos.newAtom(atom_name) catch null;
+        atom.?.traceFmt(@src(), "-----> {s}", .{atom_name}, .{});
+        defer {
+            atom.?.traceFmt(@src(), "<----- {s}", .{atom_name}, .{});
+            atom.?.destroy();
+        }
+
+        const libmsquic_path = std.posix.getenv("LIBMSQUIC_PATH") orelse {
+            atom.?.err(@src(), "Env variable LIBMSQUIC_PATH is not set", .{});
+            return error.LibPathNotSet;
+        };
+        atom.?.infoFmt(@src(), "Try to load: {s}", .{libmsquic_path}, .{});
+
+        const new_msquic = try allocator.create(MsQuic);
+        errdefer allocator.destroy(new_msquic);
+
+        new_msquic.* = MsQuic{};
+        try new_msquic.init(libmsquic_path);
+        errdefer new_msquic.deinit();
+        atom.?.debug(@src(), "MsQuic library loaded successfully", .{});
+
+        return Server{
+            .allocator = allocator,
+            .cosmos = new_cosmos,
+            .msquic = new_msquic,
+        };
+    }
+
+    fn deinit(self: *Server) void {
+        {
+            const atom = self.enter(@src(), "Server.deinit");
+            defer self.leave(@src(), atom);
+
+            self.msquic.deinit();
+            self.allocator.destroy(self.msquic);
+            atom.?.debug(@src(), "MsQuic library unloaded", .{});
+        }
+
+        self.cosmos.destroy();
+    }
+
+    fn enter(self: *Server, loc: Location, name: []const u8) ?*Atom {
+        const atom = self.cosmos.newAtom(name) catch { return null; };
+        atom.traceFmt(loc, "-----> {s}", .{name}, .{});
+        return atom;
+    }
+
+    fn leave(self: *Server, loc: Location, atom: ?*Atom) void {
+        _ = self;
+        if (atom) |a| {
+            a.traceFmt(loc, "<----- {s}", .{a.name}, .{});
+            a.destroy();
+        }
+    }
+
+    fn run(self: *Server) void {
+        const atom = self.enter(@src(), "Server.run");
+        defer self.leave(@src(), atom);
+
+        atom.?.notice(@src(), "Running!", .{});
+    }
+};
+
+pub fn main() !void {
+    const GPA = std.heap.GeneralPurposeAllocator(.{
+        .safety = true,
+        .retain_metadata = true,
+    });
+
+    var gpa = GPA{};
+    defer {
+        const leaked = gpa.deinit();
+        if (leaked == .leak) {
+            std.debug.print("MEMORY LEAK DETECTED!\n", .{});
+        }
+    }
+
+    var server = try Server.init(gpa.allocator());
+    defer server.deinit();
+    server.run();
+}
