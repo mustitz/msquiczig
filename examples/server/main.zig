41415d1647d6197606297cf7808a2b3061e7d4ca
Mustitz <mustitz@gmail.com>
2025-06-27 00:47:45 +0000

    Add conf.loadCred for server example.

--- examples/server/main.zig
+++ examples/server/main.zig
@@ -11,9 +11,17 @@ const MsQuic = msquiczig.MsQuic;
 const Registration = msquiczig.Registration;
 const Configuration = msquiczig.Configuration;
 const Settings = msquiczig.Settings;
+const CredFlags = msquiczig.CredFlags;
+const CertFile = msquiczig.CertFile;
+const CertFileProtected = msquiczig.CertFileProtected;
+const CredConfig = msquiczig.CredConfig;
 
 const IDLE_TIMEOUT = 1000;
 const ALPNS = [_][]const u8{ "sample"};
+
+const CERT_PATH = "tls/server.cert";
+const KEY_PATH = "tls/server.key";
+const PASSWORD = "";
 
 var stdout_tracer = quarkz.cosmos.FileRecorder{
     .min_level = .trace,
@@ -68,6 +76,34 @@ const Server = struct {
         const new_conf = try new_reg.openConf(&ALPNS, &settings, null);
         errdefer new_conf.close();
         atom.?.debug(@src(), "Configuration opened successfully", .{});
+
+        const has_password = PASSWORD.len > 0;
+        var cred = CredConfig{
+            .cred_type = if (has_password) .cert_file else .cert_file_protected,
+            .flags = CredFlags{},
+        };
+
+        atom.?.infoFmt(@src(),
+            "Loading server certificates: cert={s}, key={s}, password={s}",
+            .{CERT_PATH, KEY_PATH, if (has_password) "yes" else "no"}, .{});
+
+        if (has_password) {
+            var cert_file = CertFile{
+                .cert_file = CERT_PATH.ptr,
+                .private_key_file = KEY_PATH.ptr,
+            };
+            cred.cred_data = &cert_file;
+        } else {
+            var cert_file_protected = CertFileProtected{
+                .cert_file = CERT_PATH.ptr,
+                .private_key_file = KEY_PATH.ptr,
+                .private_key_password = PASSWORD.ptr,
+            };
+            cred.cred_data = &cert_file_protected;
+        }
+
+        try new_conf.loadCred(&cred);
+        atom.?.debug(@src(), "Configuration credentials loaded successfully", .{});
 
         return Server{
             .allocator = allocator,
